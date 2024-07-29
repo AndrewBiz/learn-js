3ebe8d56277b6cd28962e65f564b74ba2712160e
Andrew Biz <andrew.bizyaev@gmail.com>
2024-07-29 12:49:17 +0300

    lesson 3

---
+++ JS3/js/main.js
@@ -0,0 +1,11 @@
+const myVar = "Желудиновая";
+
+console.log(myVar);
+// console.log(myVar.length);
+// console.log(myVar.charAt(1));
+// console.log(myVar.indexOf("ema"));
+// console.log(myVar.slice(5, 6));
+// console.log(myVar.toUpperCase());
+// console.log(myVar.toLowerCase());
+// console.log(myVar.includes("Жел"));
+console.log(myVar.split("и"));
