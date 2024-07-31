d9db6d05098de3bfd9428f78fd63245a0c647a8b
Andrew Biz <andrew.bizyaev@gmail.com>
2024-07-31 11:20:39 +0300

    + lesson 5

---
+++ JS5-6/js/main.js
@@ -0,0 +1,25 @@
+const myVar1 = 42;
+const myVar2 = 42.0003;
+const myVar3 = "42.0003";
+
+// console.log(Math.PI);
+// console.log(Math.trunc(3.5));
+// console.log(Math.round(3.5));
+// console.log(Math.ceil(Math.PI));
+// console.log(Math.floor(3.5));
+
+// console.log(Math.pow(2, 4));
+// console.log(Math.min(2, 0.4, 4));
+// console.log(Math.max(2, 40.4, 4));
+
+// console.log(Math.floor(Math.random() * 10 + 1));
+
+const myName = "Trulalator";
+console.log(myName.charAt(Math.floor(Math.random() * myName.length)));
+console.log(myName.charAt(Math.floor(Math.random() * myName.length)));
+console.log(myName.charAt(Math.floor(Math.random() * myName.length)));
+console.log(myName.charAt(Math.floor(Math.random() * myName.length)));
+console.log(myName.charAt(Math.floor(Math.random() * myName.length)));
+console.log(myName.charAt(Math.floor(Math.random() * myName.length)));
+console.log(myName.charAt(Math.floor(Math.random() * myName.length)));
+console.log(myName.charAt(Math.floor(Math.random() * myName.length)));
